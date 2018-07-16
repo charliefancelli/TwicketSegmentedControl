7b31b9cbc28ecffad0bf6111072a360b30ff7362
arthur <arthur.daurel@goandup.fr>
2018-07-16 16:09:31 +0200

    Remove border

--- TwicketSegmentedControl/TwicketSegmentedControl.swift
+++ TwicketSegmentedControl/TwicketSegmentedControl.swift
@@ -176,8 +176,8 @@ open class TwicketSegmentedControl: UIControl {
   
       containerView.layer.cornerRadius = cornerRadius
   
-      containerView.layer.borderWidth = 0.8
-      containerView.layer.borderColor = UIColor(red: 241/255, green: 0/255, blue: 47/255, alpha: 1.0).cgColor
+      containerView.layer.borderWidth = 0.0
+      containerView.layer.borderColor = UIColor.clear
   
       backgroundColor = .white
       backgroundView.backgroundColor = segmentsBackgroundColor
